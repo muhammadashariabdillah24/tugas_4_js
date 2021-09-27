d05cd03b2f3934a1ca92b6e8f1cadf59982fc369
Muhammad Ashari Abdillah <tutordunet09@gmail.com>
2021-09-27 08:19:55 +0700

    tugas_4_js

---
+++ Tugas-4.js
@@ -0,0 +1,22 @@
+let tinggiBadanYuda = 190
+var tinggiBadanDani = 160
+const tinggiBadanDidi = 150
+
+if (tinggiBadanYuda >= 180) {
+    
+    console.log(
+        `Tinggi Badan Yuda : ${tinggiBadanYuda}`
+    );
+    if (tinggiBadanDani <= 170) {
+        
+        console.log(
+            `Tinggi Badan Dani : ${tinggiBadanDani}`
+        );
+        if (tinggiBadanDidi >= 140) {
+            
+            console.log(
+                `Tinggi Badan Didi : ${tinggiBadanDidi}`
+            );
+        }
+    }
+} 
